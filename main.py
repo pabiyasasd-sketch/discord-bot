fa5a2c9f857ef23191c8ad2985e848dfbcb59895
pabiyasasd-sketch <p.abiyasasd@gmail.com>
2026-03-27 18:41:49 +0700

    update to send img

--- main.py
+++ main.py
@@ -47,4 +47,27 @@ async def heh(ctx, count: int = 5):
 async def lol(ctx, count: int = 5):
     await ctx.send("LO" * count)
 
+@bot.command()
+async def cat(ctx):
+    # Send a cat picture from the local directory
+    with open('img2/cat.jpg', 'rb') as f:
+        picture = discord.File(f)
+    # Send the picture to the channel
+    await ctx.send(file=picture)
+
+@bot.command()
+async def mem(ctx):
+    # Send a meme picture from the local directory
+    memes = os.listdir('img')
+    with open(random.choice([f'img/{meme}' for meme in memes]), 'rb') as f:
+        picture = discord.File(f)
+    # Send the picture to the channel
+    await ctx.send(file=picture)
+
+@bot.command('duck')
+async def duck(ctx):
+    # Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
 bot.run("*** no ***")
